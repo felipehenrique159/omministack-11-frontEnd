86def88123cd9e0a6f417224a8093d30bbca096d
felipehenrique159 <felipecod6@hotmail.com>
2020-08-18 11:36:08 -0300

    estrutura pagina de login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
-
+import React , {useState} from 'react';
+import Logon from  './pages/Logon'
+import './global.css'
 
 function App() {
+
   return (
-     <h1>Hello World</h1>
+     <div>
+       <Logon></Logon>
+     </div>
   );
 }
 
