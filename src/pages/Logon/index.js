4515dc7da6193379a3c2b8de824c1d454dc733eb
felipehenrique159 <felipecod6@hotmail.com>
2020-08-31 15:04:09 -0300

    adicionado componente toast para alertas

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -4,6 +4,8 @@ import {FiLogIn} from 'react-icons/fi'
 import './styles.css'
 import logoIMG from '../../assets/logo.svg'
 import heroesIMG from '../../assets/heroes.png'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export default function Logon(){
     return(
       <div className="logon-container">
@@ -21,7 +23,20 @@ export default function Logon(){
             </form>
         
           </section>
-        
+
+          
+          <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                />
+                <ToastContainer />
 
           <img src={heroesIMG} alt="Heroes"/>
       </div>
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,9 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import logoIMG from '../../assets/logo.svg'
 import {Link , useHistory} from 'react-router-dom'
 import {FiArrowLeft} from 'react-icons/fi'
+import { toast,ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import api from '../../services/api'
 
 import './style.css';
@@ -13,9 +15,18 @@ export default function Register(){
     const [whatsapp,setWhatsapp] = useState('')
     const [city,setCity] = useState('')
     const [uf,setUf] = useState('')
-
+    // const [ufs,setUfs] = useState('')
     const history = useHistory()
-
+    
+    
+    // useEffect(() =>{
+    //     api.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(res=>{
+    //       const ufs = res.data.map(uf => uf.sigla)
+    //      setUfs(ufs)
+    //     })
+    // },[])
+    
+    
     async function handleRegister(e){
         e.preventDefault();
        const data = {
@@ -28,10 +39,45 @@ export default function Register(){
 
        try {
         const res = await api.post('ongs',data)
-        alert(`Seu ID de acesso: ${res.data.id}`);
-        history.push('/')
+        console.log(res)
+        if(res.data != null || res.data != undefined){     
+            // alert(`Seu ID de acesso: ${res.data.id}`);
+            console.log(res.data);
+            // toast.info('ONG cadastrada com sucesso!', {
+            //     position: "top-right",
+            //     autoClose: 5000,
+            //     hideProgressBar: false,
+            //     closeOnClick: true,
+            //     pauseOnHover: true,
+            //     draggable: true,
+            //     progress: undefined,
+            // });
+            toast.info(`Anote seu ID de acesso: ${res.data}`, {
+                position: "top-right",
+                // autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+
+            history.push('/') //redireciona pra tela de logon
+        
+        }
+       
        } catch (error) {
-            alert('Erro no cadastro, tente novamente!')   
+            console.log(error)
+            console.log('erro ao gravar')
+            toast.error('Erro no cadastro, tente novamente!', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                }); 
        }
     }
 
@@ -52,23 +98,27 @@ export default function Register(){
             <form onSubmit={handleRegister}>
             
                  <input placeholder="Nome da ONG" type="text" 
-                 value={name} onChange={e => setName(e.target.value)}/>
+                 value={name} onChange={e => setName(e.target.value)} required/>
               
                 <input placeholder="Email" type="email" 
-                value={email} onChange={e => setEmail(e.target.value)}/>
+                value={email} onChange={e => setEmail(e.target.value)} required/>
                
                 <input placeholder="Whatsapp" type="text" 
-                value={whatsapp} onChange={e => setWhatsapp(e.target.value)}/>
+                value={whatsapp} onChange={e => setWhatsapp(e.target.value)} required/>
               
                 <div className="input-group">
-                    <input placeholder="Cidade" type="text"  value={city} onChange={e => setCity(e.target.value)}/>
-                    <input placeholder="UF" type="text" style={{width: 80}}  value={uf} onChange={e => setUf(e.target.value)}/>
+                    <input placeholder="Cidade" type="text"  value={city} onChange={e => setCity(e.target.value)} required/>
+                    <input placeholder="UF" type="text" style={{width: 80}}  value={uf} onChange={e => setUf(e.target.value)} required/>
                 </div>
-
+              
+                
                 <button className="button" type="submit" placeholder="Cadastrar">
                     Cadastrar
                 </button>
             </form>
+
+            
+           
         </div>
     </div>
     )
